56bfe1e22a80056bfe5ef8665c2c0be51cbab22e
aapatni <aapatni@stu.naperville203.org>
2017-05-12 13:18:57 -0500

    Line follow workds

--- LineFollow.java
+++ LineFollow.java
@@ -1,12 +1,19 @@
 import lejos.nxt.*;
+import lejos.robotics.navigation.DifferentialPilot;
 
 public class LineFollow{
     public static void main(String[] args){
-        ColorSensor c = new ColorSensor(SensorPort.S1,1);
-        System.out.println(c.isFloodlightOn());
-        while (true){
-           
-            LCD.drawInt(c.getLightValue(),0,3);
+        LightSensor right = new LightSensor(SensorPort.S1);
+        DifferentialPilot pilot = new DifferentialPilot(56,20.5,Motor.A,Motor.B);
+        
+        pilot.setTravelSpeed(150);
+        
+        
+        while(true){
+           pilot.steer((right.getNormalizedLightValue()-400)*.75);
+           System.out.println(right.getNormalizedLightValue());
+           if(Button.ENTER.isPressed()){break;}
         }
     }
+    
 }
